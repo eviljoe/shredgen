85521a4b864bb537798d5b49dafb2934f81a8a52
eviljoe <eviljoe@users.noreply.github.com>
2018-03-27 21:37:30 -0400

    Add command line option parsing
    Add options for...
     * Showing all the available scales
     * Showing all the available scale names

--- src/shredgen.py
+++ src/shredgen.py
@@ -1,16 +1,98 @@
 #!/usr/bin/env python3
 
 
+import argparse
+import sys
+
+
 _NOTES_IN_OCTAVE = 12
+
+_ERR_NO_SCALE_SPECIFIED = 2
+_ERR_UNKNOWN_SCALE = 3
 
 
 def main():
-    for scale in _create_major_pentatonic_scales():
-        atab = ASCIITab(scale.notes)
-        print('{}\n{}\n\n'.format(scale.name, atab), end='')
+    opts = _parse_opts()
+    err = _perform_user_action(opts)
+    sys.exit(0 if err is None else err)
 
 
-def _create_major_pentatonic_scales():
+def _parse_opts():
+    parser = argparse.ArgumentParser(description='Generate boring rifts that solidify faces')
+
+    # Positional Arguments
+    parser.add_argument('scale', nargs='?', metavar='scale', default='AMajPen',
+                        help='The scale to be used (default: %(default)s)')
+
+    # Optional Arguments
+    parser.add_argument('--all-scales', action='store_true', default=False, dest='all_scales',
+                        help='Display all possible scales')
+    parser.add_argument('--all-scale-names', action='store_true', default=False, dest='all_scale_names',
+                        help='Display all possible scale names')
+
+    # TODO add --length option to specify how many notes generate when shredding
+
+    return parser.parse_args()
+
+
+def _perform_user_action(opts):
+    err = None
+
+    if opts.all_scales:
+        _display_all_scales()
+    elif opts.all_scale_names:
+        _display_all_scale_names()
+    else:
+        err = _shred(opts)
+
+    return err
+
+
+def _display_all_scales():
+    print('\n\n'.join(['{}\n{}'.format(scale.name, ASCIITab(scale.notes)) for scale in _get_all_scales()]))
+
+
+def _display_all_scale_names():
+    print('\n'.join(['{} ({})'.format(scale.name, ', '.join(scale.aliases)) for scale in _get_all_scales()]))
+
+
+def _shred(opts):
+    err = None
+    scale_name = opts.scale.strip().lower() if opts.scale else ''
+    scale = None
+
+    if len(scale_name) == 0:
+        err = _ERR_NO_SCALE_SPECIFIED
+        # TODO todo print err msg
+
+    if err is None:
+        scale = _get_scale_by_name(scale_name)
+
+        if not scale:
+            err = _ERR_UNKNOWN_SCALE
+            # TODO todo print err msg
+
+    if err is None:
+        _shred_in_scale(scale)
+
+    return err
+
+
+def _shred_in_scale(scale):
+    # TODO todo shred in scale
+    print('TODO: shred in this scale...')
+    print(str(ASCIITab(scale.notes)))
+
+
+def _get_scale_by_name(name):
+    return next((scale for scale in _get_all_scales() if name in [alias.lower() for alias in scale.aliases]), None)
+
+
+def _get_all_scales():
+    return _get_major_pentatonic_scales()
+
+
+def _get_major_pentatonic_scales():
     a_maj_pen = MajorPentatonicScale('A', [
         Note('E', 5), Note('E', 6), Note('E', 8),
         Note('A', 5), Note('A', 7), Note('A', 8),
@@ -20,20 +102,20 @@ def _create_major_pentatonic_scales():
         Note('e', 5), Note('e', 6), Note('e', 8),
     ])
 
-    scales = [a_maj_pen]
-    scales.append(MajorPentatonicScale.from_other(a_maj_pen, 1, 'A#'))
-    scales.append(MajorPentatonicScale.from_other(a_maj_pen, 2, 'B'))
-    scales.append(MajorPentatonicScale.from_other(a_maj_pen, 3, 'C'))
-    scales.append(MajorPentatonicScale.from_other(a_maj_pen, 4, 'C#', wrap=False))
-    scales.append(MajorPentatonicScale.from_other(a_maj_pen, 5, 'D', wrap=False))
-    scales.append(MajorPentatonicScale.from_other(a_maj_pen, 6, 'D#', wrap=False))
-    scales.append(MajorPentatonicScale.from_other(a_maj_pen, 7, 'E'))
-    scales.append(MajorPentatonicScale.from_other(a_maj_pen, 8, 'F'))
-    scales.append(MajorPentatonicScale.from_other(a_maj_pen, 9, 'F#'))
-    scales.append(MajorPentatonicScale.from_other(a_maj_pen, 10, 'G'))
-    scales.append(MajorPentatonicScale.from_other(a_maj_pen, 11, 'G#'))
-
-    return scales
+    return [
+        a_maj_pen,
+        MajorPentatonicScale.from_other(a_maj_pen, 1, 'A#'),
+        MajorPentatonicScale.from_other(a_maj_pen, 2, 'B'),
+        MajorPentatonicScale.from_other(a_maj_pen, 3, 'C'),
+        MajorPentatonicScale.from_other(a_maj_pen, 4, 'C#', wrap=False),
+        MajorPentatonicScale.from_other(a_maj_pen, 5, 'D', wrap=False),
+        MajorPentatonicScale.from_other(a_maj_pen, 6, 'D#', wrap=False),
+        MajorPentatonicScale.from_other(a_maj_pen, 7, 'E'),
+        MajorPentatonicScale.from_other(a_maj_pen, 8, 'F'),
+        MajorPentatonicScale.from_other(a_maj_pen, 9, 'F#'),
+        MajorPentatonicScale.from_other(a_maj_pen, 10, 'G'),
+        MajorPentatonicScale.from_other(a_maj_pen, 11, 'G#'),
+    ]
 
 
 class Note:
